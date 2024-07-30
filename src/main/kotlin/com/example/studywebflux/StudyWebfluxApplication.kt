c51fd676f511188b32352fbe15d0c29ef8b7a106
changu100 <chad@101.inc>
2024-07-30 23:22:21 +0900

    feat: init test

---
+++ src/main/kotlin/com/example/studywebflux/StudyWebfluxApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studywebflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyWebfluxApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyWebfluxApplication>(*args)
+}
